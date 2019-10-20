06b8be2fdd500787853f7f2453a8be1d897dea06
Jorge Rodriguez <jorgecrb@gmail.com>
2019-10-20 16:26:32 -0500

    Lookup on run

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-(function livetranscript() {
-    const REFRESH_TIME = 1000;
 
-    let parentElement = document.getElementsByClassName('rc-InteractiveTranscript')[0];
+const REFRESH_TIME = 1500;
+
+let transcriptScrollInterval = setInterval(run, REFRESH_TIME);
+let parentElement;
+
+function setupScrollBox() {
+    parentElement.style = 'overflow:auto; height: 45vh;';
+    parentElement.addEventListener('scroll', (ev) => ev.preventDefault());
+}
+
+function getActiveElement() {
+    var active = parentElement.querySelector('.rc-Phrase.active');
+    return active;
+}
+
+function scrollToElement(element) {
+    element.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+}
+
+function run() {
+    parentElement = document.getElementsByClassName('rc-InteractiveTranscript')[0];
+    if (!parentElement) return;
+
     setupScrollBox();
-    let transcriptScrollInterval = setInterval(run, REFRESH_TIME);
-
-    function setupScrollBox() {
-        parentElement.style = 'overflow:auto; height: 45vh;';
-        parentElement.addEventListener('scroll', (ev) => ev.preventDefault());
-    }
-
-    function getActiveElement() {
-        var active = parentElement.querySelector('.rc-Phrase.active');
-        return active;
-    }
-
-    function scrollToElement(element) {
-        element.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
-    }
-
-    function run() {
-        scrollToElement(getActiveElement())
-    }
-
-    return transcriptScrollInterval;
-})();
+    scrollToElement(getActiveElement());
+}
